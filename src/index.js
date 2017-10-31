702da9bc77481d742bf98b38abc8d14826b5560b
Ishani Mukherjee <ishanibhattacharjee@gmail.com>
2017-10-30 18:04:54 -0700

    added redux store with createStore in index.js file

--- src/index.js
+++ src/index.js
@@ -5,17 +5,10 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
 import reducer from './reducers'
-import { Provider } from 'react-redux'
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = createStore(reducer)
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+console.log(store)
+
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
